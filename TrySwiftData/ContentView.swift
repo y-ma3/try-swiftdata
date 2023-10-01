cf7ec2a3601dab7245687c785f993585c7c969c8
Tsuri Yuma <yuma33t@gmail.com>
2023-10-02 00:14:05 +0900

    Initial Commit

---
+++ TrySwiftData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrySwiftData
+//
+//  Created by 釣悠馬 on 2023/10/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrySwiftData/TrySwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrySwiftDataApp.swift
+//  TrySwiftData
+//
+//  Created by 釣悠馬 on 2023/10/02.
+//
+
+import SwiftUI
+
+@main
+struct TrySwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
